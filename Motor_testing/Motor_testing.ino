e57a35c9ca9a58f5079868c303e3caf16c84c399
Nam Truong <nam.truongph@gmail.com>
2017-07-10 11:29:35 +0700

    Version 1

--- Motor_testing/Motor_testing.ino
+++ Motor_testing/Motor_testing.ino
@@ -4,12 +4,27 @@
 //#define sec 1300000
 
 unsigned long count;
-int Motor;
+int For = 0b01100000;
+int Back = 0b10010000;
+int Release = 0b00000000;
+int Left = 0b10100000;
+int Right = 0b01010000;
 
 void setup () {
   Serial.begin (9600);
   DDRD = 0b11110000;
-  Motor = 0b00000000;
+}
+
+void setPWM6 (uint8_t PWM6){
+  TCCR0A = 0b10000011;
+  TCCR0B = 0b00000001;
+  OCR0A = PWM6;
+}
+
+void setPWM5 (uint8_t PWM5){
+  TCCR1A = 0b00100011;
+  TCCR1B = 0b00000001;
+  OCR1B = PWM5;
 }
 
 void delay_ms (uint16_t millisecond) {
@@ -18,22 +33,57 @@ void delay_ms (uint16_t millisecond) {
   for (count = 0; count < sec; count ++);
 }
 
+void Forward (){
+  PORTD = For;
+}
+
+void Backward (){
+  PORTD = Back;
+}
+
+void Stop (){
+  PORTD = Release;
+}
+
+void TurnLeft (){
+  PORTD = Left;
+}
+
+void TurnRight (){
+  PORTD = Right;
+}
+
 
 void loop () {
-Motor = 0b10000000;
-PORTD = Motor;
-Serial.print ("For:  ");
-Serial.println (PORTD);
-delay(1000);
-Motor = Motor + 0b11000000;
-PORTD = Motor; 
-Serial.print ("Back:  ");
-Serial.println (PORTD);
-delay(1000);
-Motor = Motor + 0b01000000;
-PORTD = Motor; 
-Serial.print ("Back:  ");
-Serial.println (PORTD);
-delay(1000);
+  Forward;
+  delay_ms (1000);
+  Backward;
+  delay_ms(1000);
+  Forward;
+  delay_ms(1000);
+  TurnLeft;
+  delay_ms(500);
+  TurnRight;
+  delay_ms(500);
+
+  int a;
+  for (a = 0; a <= 255; a++){
+  setPWM6 (a);
+  Forward;
+  delay_ms (10);
+  }
+
+  for (a = 0; a <= 255; a++){
+  setPWM5 (a);
+  Forward;
+  delay_ms (10);
+  }
+
+  for (a = 0; a <= 255; a++){
+  setPWM5 (a);
+  setPWM6 (a);
+  Forward;
+  delay_ms (10);
+  }
 }
 
