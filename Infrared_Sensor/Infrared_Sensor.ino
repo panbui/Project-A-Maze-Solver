a9de4db9bb4a29f09fb5165653f0325dd1072d6f
Nam Truong <nam.truongph@gmail.com>
2017-07-11 15:44:09 +0700

    Sensor

---
+++ Infrared_Sensor/Infrared_Sensor.ino
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <avr/io.h>
+int sensor[2]={0, 0};
+
+void setup() {
+
+Serial.begin (9600);
+DDRB = 0b00000000;
+
+}
+
+void loop() {
+
+//Serial.print ("sensor1: ");
+if (PINB & (1<<PINB5)){
+//  Serial.println ("1");
+  sensor[0] = 1;
+}else{
+//  Serial.println ("0");
+    sensor[0] = 0;
+}
+
+//Serial.print ("sensor2: ");
+if (PINB & (1<<PINB4)){
+//  Serial.println ("1");
+    sensor[1] = 1;
+}else{
+//  Serial.println ("0");
+    sensor[1] = 0;
+}
+
+Serial.print (sensor[0]);
+Serial.print ("   ");
+Serial.println (sensor[1]);
+if (sensor[0] == 1 && sensor[1] == 1){
+  Serial.println ("Yes");
+}else{
+  Serial.println ("No");
+}
+
+delay (500);
+
+  
+}
